3b34782afc7b0bfab2c0d76cef5db36228551f3c
shjeng <ghsmsl20@gmail.com>
2024-11-12 23:06:57 +0900

    feat BaseEntity 추가

---
+++ src/main/java/com/my/cook_recipe/common/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.my.cook_recipe.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime localDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDateTime;
+
+    @Column
+    private String writer;
+    @Column
+    private String modifier;
+}
